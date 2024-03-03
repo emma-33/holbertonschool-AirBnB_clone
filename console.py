1f4018158b535a1ca91f18f2a8d3c4b4c22ccdf2
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-03-03 14:59:53 +0100

    Projet AirBnb Ajout console hbnb Gab

--- console.py
+++ console.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python3
 """ console that contains the entry point of the command interpreter """
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Create class HBNB"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Exit program"""
+        print("quit")
+        return (True)
+    
+    def do_EOF(self, arg):
+        """Exit program"""
+        print("quit")
+        return (True)
+    
+    def emptyline(self):
+        """Nothing emptyline"""
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
