64e907b7d37e9a38e1f624e0bb3e61d923407b1f
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-03-04 12:43:00 +0100

    projet Airbnb Ajout create console

--- console.py
+++ console.py
@@ -22,5 +22,19 @@ class HBNBCommand(cmd.Cmd):
         """Nothing emptyline"""
         pass
 
+    def do_create(self, arg):
+        """Create a new instance"""
+        if not arg:
+            print("** class missing **")
+        
+        else:
+            try:
+                class_instance = globals()[arg]()
+                class_instance.save()
+                print(class_instance.id)
+            
+            except KeyError:
+                print("** class doesn't exist")
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
