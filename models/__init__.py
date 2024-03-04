2e4d86f00e6027d324057da1cf11128186de4027
root <root@LAPTOP-2LV4F0PM>
2024-03-04 11:26:09 +0100

    storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Initialization file to create a unique FileStorage instance"""
-
-
 from models.engine.file_storage import FileStorage
 
-file_storage = FileStorage()
-file_storage.reload()
+file_path ="file.json"
+storage = FileStorage(file_path)
+storage.reload()
