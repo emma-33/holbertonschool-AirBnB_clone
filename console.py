845c212f336e4a899cc6f83be1ac484f8384ddbe
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-03-06 01:08:01 +0100

    projet airbnb finition de la console ajout de add/all/update + fix problem

--- console.py
+++ console.py
@@ -2,6 +2,9 @@
 """ console that contains the entry point of the command interpreter """
 
 import cmd
+from models.base_model import BaseModel
+from models.file_storage import FileStorage
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -61,6 +64,111 @@ class HBNBCommand(cmd.Cmd):
             
             else:
                 print(BaseModel.instances[instance_key].to_dict())
+    
+    def do_destroy(self, arg):
+        """Destroy instance"""
+        args = arg.split()
+
+        if not args:
+            print("** class name missing **")
+        
+        else:
+            class_name = args[0]
+
+            if class_name not in globals():
+                print("** class doesn't exist **")
+            
+            elif len(args) < 2:
+                print("** instance id missing **")
+            
+            else:
+                instance_id = args[1]
+                storage = FileStorage()
+                storage.reload()
+                instance_key = "{}.{}".format(class_name, instance_id)
+                
+                if instance_key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[instance_key]
+                    storage.save()
+    
+    def do_all(self, arg):
+        """Display all instance from json file"""
+        storage = FileStorage()
+        storage.reload()
+
+        if not arg:
+            instance_list = list(storage.all().values())
+        
+        else:
+            class_name = arg
+            
+            if class_name not in globals():
+                print("** class doesn't exist **")
+                return
+            
+            else:
+                instance_list = [value for key, value in storage.all().items() if key.startswith(class_name + ".")]
+
+        if not instance_list:
+            print("** no instance found **")
+        
+        else:
+            for instance in instance_list:
+                print(instance.to_dict())
+    
+    
+    def do_update(self, arg):
+        """Update an instance with id and class information"""
+
+        args = arg.split()
+        storage = FileStorage()
+        storage.reload()
+
+        if not args:
+            print("** class name missing **")
+        
+        else:
+            class_name = args[0]
+            
+            if class_name not in globals():
+                print("** class doesn't exist **")
+            
+            elif len(args) < 2:
+                print("** instance id missing **")
+            
+            else:
+                instance_id = args[1]
+                instance_key = "{}.{}".format(class_name, instance_id)
+                
+                if instance_key not in storage.all():
+                    print("** no instance found **")
+                
+                elif len(args) < 3:
+                    print("** attribute name missing **")
+                
+                elif len(args) < 4:
+                    print("** value missing **")
+                
+                else:
+                    attribute_name = args[2]
+                    attribute_value = args[3]
+                    instance = storage.all()[instance_key]
+
+                    if hasattr(instance, attribute_name):
+                        attribute_type = type(getattr(instance, attribute_name))
+                        try:
+                            casted_value = attribute_type(attribute_value)
+                        except ValueError:
+                            print("** invalid value type **")
+                            return
+
+                        setattr(instance, attribute_name, casted_value)
+                        storage.save()
+                    else:
+                        print("** attribute doesn't exist **")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
