6d641f6bd0927ea41a657940ba19fbcf51c057e2
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-03-06 17:42:16 +0100

    pycodestyle console

--- console.py
+++ console.py
@@ -30,12 +30,12 @@ class HBNBCommand(cmd.Cmd):
         """Exit program"""
         print("quit")
         return (True)
-    
+
     def do_EOF(self, arg):
         """Exit program"""
         print("quit")
         return (True)
-    
+
     def emptyline(self):
         """Nothing emptyline"""
         pass
@@ -44,71 +44,71 @@ class HBNBCommand(cmd.Cmd):
         """Create a new instance"""
         if not arg:
             print("** class missing **")
-        
+
         else:
             try:
                 class_instance = globals()[arg]()
                 class_instance.save()
                 print(class_instance.id)
-            
+
             except KeyError:
                 print("** class doesn't exist **")
-    
+
     def do_show(self, arg):
         """display representation of instance"""
         args = arg.split()
-        
+
         if not args:
             print("** class name missing **")
             return
-        
+
         else:
             class_name = args[0]
-        
+
         if class_name not in globals():
             print("** class doesn't exist **")
-        
+
         elif len(args) < 2:
             print("** instance id missing **")
-        
+
         else:
             instance_id = args[1]
             instance_key = f"{class_name}.{instance_id}"
-            
+
             if instance_key not in storage.all():
                 print("** no instance found **")
-            
+
             else:
                 print(storage.all()[instance_key].to_dict())
-    
+
     def do_destroy(self, arg):
         """Destroy instance"""
         args = arg.split()
 
         if not args:
             print("** class name missing **")
-        
+
         else:
             class_name = args[0]
 
             if class_name not in globals():
                 print("** class doesn't exist **")
-            
+
             elif len(args) < 2:
                 print("** instance id missing **")
-            
+
             else:
                 instance_id = args[1]
                 storage = FileStorage()
                 storage.reload()
                 instance_key = "{}.{}".format(class_name, instance_id)
-                
+
                 if instance_key not in storage.all():
                     print("** no instance found **")
                 else:
                     del storage.all()[instance_key]
                     storage.save()
-    
+
     def do_all(self, arg):
         """Display all instance from json file"""
         storage = FileStorage()
@@ -116,56 +116,58 @@ class HBNBCommand(cmd.Cmd):
 
         if not arg:
             instance_list = list(storage.all().values())
-        
+
         else:
             class_name = arg
-            
+
             if class_name not in globals():
                 print("** class doesn't exist **")
                 return
-            
+
             else:
-                instance_list = [value for key, value in storage.all().items() if key.startswith(class_name + ".")]
+                instance_list = [
+                    value for key, value in storage.all().items()
+                    if key.startswith(class_name + ".")]
 
         if not instance_list:
             print("** no instance found **")
-        
+
         else:
             for instance in instance_list:
                 print(instance.to_dict())
-    
-    
+
+
     def do_update(self, arg):
         """Update an instance with id and class information"""
-
         args = arg.split()
 
         if not args:
             print("** class name missing **")
-        
+
         elif args[0] not in self.class_name:
             print("** class doesn't exist **")
-        
+
         elif len(args) < 2:
             print("** instance id missing **")
-        
+
         elif len(args) < 3:
             print("** attribute name missing **")
-        
+
         elif len(args) < 4:
             print("** value missing **")
-        
+
         else:
             key = "{}.{}".format(args[0], args[1])
             all_objects = storage.all()
             obj = all_objects.get(key)
-        
+
             if obj:
                 setattr(obj, args[2], eval(args[3]))
                 storage.save()
-        
+
             else:
                 print("** no instance found **")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
